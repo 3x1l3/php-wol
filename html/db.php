25b74c64e13b5ae035b1202dafbc67fb28711ae5
Chad Klassen <chadklassen1@gmail.com>
2017-12-21 00:08:24 -0700

    Build will probably fail
    In the middle of restructure.

--- html/db.php
+++ html/db.php
@@ -1,34 +1,48 @@
 <?php
-class Database extends mysqli{
-	private $servername = "localhost";
-	private $username = "root";
-	private $password = "";
-	private $database = "php-wol";
-	private $mysqli;
-	
-	function __construct() {
-		// Create connection
-		parent::__construct($this->servername, $this->username, $this->password);
 
-		// Check connection
-		if ($this->connect_error) {
-			die("Connection failed: " . $this->connect_error);
-		}
-		
-		//check if database exists
-		$result = $this->query("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '{$this->database}'");
-		if(mysqli_num_rows($result) <= 0) $this->create();
-		else $this->query("USE `{$this->database}`");
-	}
-	
-	/**
-	 * creates a new database
-	 */
-	function create() {
-		$query = "
-CREATE DATABASE `{$this->database}`;
+class Database extends mysqli
+{
+    private $servername = "localhost";
+    private $username = "root";
+    private $password = "j";
+    private $database = "php-wol";
+    private $mysqli;
+
+    function __construct()
+    {
+        // Create connection
+        parent::__construct($this->servername, $this->username, $this->password);
+
+        // Check connection
+        if ($this->connect_error) {
+            die("Connection failed: " . $this->connect_error);
+        }
+
+        //check if database exists
+        $result = $this->query("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '{$this->database}'");
+        if (mysqli_num_rows($result) <= 0) $this->create();
+        else $this->query("USE `{$this->database}`");
+    }
+
+    public function loadConfig($filePath)
+    {
+
+        $configs = include($filePath);
+        foreach ($configs as $key => $val) {
+            $this->$key = $val;
+        }
+
+    }
+
+    /**
+     * creates a new database
+     */
+    function create()
+    {
+        $query = "
+CREATE DATABASE IF NOT EXISTS `{$this->database}`;
 USE `{$this->database}`;
-CREATE TABLE server (
+CREATE TABLE IF NOT EXISTS server (
   id varchar(45) NOT NULL PRIMARY KEY,
   name varchar(30) NOT NULL,
   ip varchar(16) NOT NULL,
@@ -36,7 +50,7 @@ CREATE TABLE server (
   broadcast varchar(16)
 );
 
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
   id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
   username varchar(30) NOT NULL UNIQUE,
   password varchar(128) NOT NULL,
@@ -45,6 +59,6 @@ CREATE TABLE user (
 
 INSERT INTO user VALUES (1, 'admin', 'f21c36829f29ba240b1de7d9487b5aa9bacf4e7309f8a61c84591e4f0642cf16fa804d66613778e315b64f377ceae352eb3c6be44073c15e7775983c69763a3a', 3);
 		";
-		$this->multi_query($query);
-	}
+        $this->multi_query($query);
+    }
 }
